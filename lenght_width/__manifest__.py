9c848b2f9f0280bc19b8004157ac220184399224
hamdy <mohamedhamdy123890456@gmail.com>
2021-01-06 17:07:54 +0200

    lenght_width

---
+++ lenght_width/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name' : 'lenght_width',
+    'version' : '1.0',
+    'category': 'lenght_width',
+    'depends' : ['base','product','stock'],
+    'data': [
+        "security/ir.model.access.csv",
+        "views/view.xml",
+        "views/product_thickness.xml",
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
