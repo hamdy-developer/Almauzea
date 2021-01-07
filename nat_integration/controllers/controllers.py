2a5ddf4a6f0d5f1a3c0cad879bfb71fd8a55ecea
hamdy <mohamedhamdy123890456@gmail.com>
2021-01-07 02:52:11 +0200

    edit

--- nat_integration/controllers/controllers.py
+++ nat_integration/controllers/controllers.py
@@ -12,20 +12,175 @@ HEADERS = {'Content-Type': 'application/json'}
 
 class NatApi(http.Controller):
 
-
     @http.route('/api/check/customer', type='json', methods=['POST'], auth='public', sitemap=False)
     def check_customer(self, **kw):
-        print('data',kw)
+        """{
+            "params": {
+                "mobile_number": "0100"
+            }
+        }"""
         if not kw:
             response = {"result": {"code": 401, "message": "All required data are missing!"}}
             return response
-        if kw.get('mobile_number',False):
-            custome = request.env['res.partner'].sudo().search([('mobile', '=', kw.get('mobile_number','False'))],limit=1)
+        else:
+            if kw.get('mobile_number', False):
+                custome = request.env['res.partner'].sudo().search([('phone', '=', kw.get('mobile_number', 'False'))],
+                                                                   limit=1)
 
-            if custome:
-                response = {"result": {"code": 200, "message": "Custome already exist",}}
+                if custome:
+                    response = {"result": {"code": 200, "message": "Custome already exist", }}
+                    return response
+                else:
+                    response = {"result": {"code": 404, "message": "Custome Not Exist"}}
+                return response
+
+    @http.route('/api/create/customer', type='json', methods=['POST'], auth='public', sitemap=False)
+    def create_customer(self, **kw):
+        """{
+                    "params": {
+                        "name": "tttttttt",
+                        "password": "123",
+                        "shop_name": "shop_name",
+                        "email": "email",
+                        "mobile": "0100",
+                        "city": "city",
+                        "zip": "zip",
+                        "street": "street",
+                        "street2": "street2"
+                    }
+                }"""
+        if not kw:
+            response = {"result": {"code": 401, "message": "All required data are missing!"}}
+            return response
+        else:
+            if kw.get('name', False) and kw.get('password', False):
+
+                vals = {
+                    'is_company': False,
+                    'customer_rank': 1,
+                    'type': 'private',
+                    'name': kw.get('name'),
+                    'shop_name': kw.get('shop_name'),
+                    'email': kw.get('email'),
+                    'phone': kw.get('mobile'),
+                    'city': kw.get('city'),
+                    'zip': kw.get('zip'),
+                    'street': kw.get('street'),
+                    'street2': kw.get('street2'),
+                    'password': kw.get('password'),
+                }
+                new_customer = request.env['res.partner'].sudo().create(vals)
+                new_customer.sudo().generate_token()
+                response = {"result": {"code": 200, "token": new_customer.token}}
                 return response
             else:
-                response = {"result": {"code": 404, "message": "Custome Not Exist"}}
-            return response
+                response = {"result": {"code": 401, "message": "password or name is missing!"}}
+                return response
 
+    @http.route('/api/edit/customer', type='json', methods=['POST'], auth='public', sitemap=False)
+    def edit_customer(self, **kw):
+        """{
+                    "params": {
+                        "token":"token",
+                        "name": "tttttttt",
+                        "password": "123",
+                        "shop_name": "shop_name",
+                        "email": "email",
+                        "mobile": "0100",
+                        "city": "city",
+                        "zip": "zip",
+                        "street": "street",
+                        "street2": "street2"
+                    }
+                }"""
+        if not kw:
+            response = {"result": {"code": 401, "message": "All required data are missing!"}}
+            return response
+        else:
+            if kw.get('name', False) and kw.get('password', False) and kw.get('token', False):
+
+                vals = {
+                    'is_company': False,
+                    'customer_rank': 1,
+                    'type': 'private',
+                    'name': kw.get('name'),
+                    'shop_name': kw.get('shop_name'),
+                    'email': kw.get('email'),
+                    'phone': kw.get('mobile'),
+                    'city': kw.get('city'),
+                    'zip': kw.get('zip'),
+                    'street': kw.get('street'),
+                    'street2': kw.get('street2'),
+                    'password': kw.get('password'),
+                }
+                customer = request.env['res.partner'].sudo().search([('token', '=', kw.get('token'))], limit=1)
+                if customer:
+                    customer.sudo().write(vals)
+                    response = {"result": {"code": 200, "Edit": "done"}}
+                    return response
+                else:
+                    response = {"result": {"code": 401, "token": "missing"}}
+                    return response
+            else:
+                response = {"result": {"code": 401, "message": "password or name or token is missing!"}}
+                return response
+
+    @http.route('/api/delete/customer', type='json', methods=['POST'], auth='public', sitemap=False)
+    def delete_customer(self, **kw):
+        """{
+                    "params": {
+                        "token":"token",
+                    }
+                }"""
+        if not kw:
+            response = {"result": {"code": 401, "message": "All required data are missing!"}}
+            return response
+        else:
+            if kw.get('token', False):
+                customer = request.env['res.partner'].sudo().search([('token', '=', kw.get('token'))], limit=1)
+                if customer:
+                    customer.sudo().unlink()
+                    response = {"result": {"code": 200, "delete": "done"}}
+                    return response
+                else:
+                    response = {"result": {"code": 401, "token": "Not Exist"}}
+                    return response
+            else:
+                response = {"result": {"code": 401, "message": "token is missing!"}}
+                return response
+
+    @http.route('/api/login/customer', type='json', methods=['POST'], auth='public', sitemap=False)
+    def login_customer(self, **kw):
+        """{
+                    "params": {
+                        "mobile": "0100",
+                        "password": "123",
+                    }
+                }"""
+        if not kw:
+            response = {"result": {"code": 401, "message": "All required data are missing!"}}
+            return response
+        else:
+            if kw.get('mobile', False) and kw.get('password', False):
+                customer = request.env['res.partner'].sudo().search([('password', '=', kw.get('password')),('phone', '=', kw.get('mobile'))], limit=1)
+                if customer:
+                    valus = {
+                        "token":  customer.token,
+                        "password": customer.password,
+                        "name": customer.name,
+                        "shop_name": customer.shop_name,
+                        "email": customer.email,
+                        "mobile": customer.phone,
+                        "city": customer.city,
+                        "zip": customer.zip,
+                        "street": customer.street,
+                        "street2": customer.street2
+                    }
+                    response = {"result": {"code": 200, "delete": valus}}
+                    return response
+                else:
+                    response = {"result": {"code": 401, "message": "Customer Not Exist"}}
+                    return response
+            else:
+                response = {"result": {"code": 401, "message": "mobile or password is missing!"}}
+                return response
--- nat_integration/models/model.py
+++ nat_integration/models/model.py
@@ -1,6 +1,8 @@
 from odoo import api, fields, models, _
-from requests import request
-import time, json, requests
+import string
+import random
+import re
+
 
 
 
@@ -9,17 +11,16 @@ class res_partner(models.Model):
     _inherit = 'res.partner'
 
     token = fields.Char(string="Token", required=False, readonly=True)
+    shop_name = fields.Char(string="Shop Name", required=False, readonly=False)
+    password = fields.Char(string="Password", required=False, readonly=False)
 
-    @api.model
-    def create(self, vals_list):
-        res = super(res_partner, self).create(vals_list)
-        for rec in res:
-            rec.generate_token()
-        return res
 
     def generate_token(self):
         for rec in self:
             rec.token = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase +
                                                string.digits, k=16))
+        partner = self.env['res.partner'].sudo().search([('token', '=', rec.token),('id', '!=', rec.id)])
+        if partner:
+            rec.generate_token()
         if not bool(re.search(r'\d', rec.token)):
             rec.generate_token()
