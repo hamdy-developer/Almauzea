704cbe5d532b9bccd4e1bf2705a979ddf9ae6481
hamdy <mohamedhamdy123890456@gmail.com>
2021-01-30 21:50:41 +0200

    test

--- almuazae_purchase/__manifest__.py
+++ almuazae_purchase/__manifest__.py
@@ -5,9 +5,9 @@
     'name' : 'almuazae_purchase',
     'version' : '1.0',
     'category': 'lenght_width',
-    'depends' : ['base','product','stock','purchase','sale'],
+    'depends' : ['base','product','stock','purchase','sale','purchase_requisition'],
     'data': [
-        # "views/view.xml",
+        "views/view.xml",
     ],
     'installable': True,
     'application': False,
